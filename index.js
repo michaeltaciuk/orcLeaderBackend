73f84b87d06c716dbc92fb50de568c434c6f4519
Michael Taciuk <taciuk73@gmail.com>
2023-03-30 14:36:50 -0400

    port taken from environment variables

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "https://");
